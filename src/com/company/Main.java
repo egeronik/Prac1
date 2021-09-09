9f3a9cd6004439509893b294e7cef5bfc95d0ac6
Egor Grebennik <eshik2002@gmail.com>
2021-09-09 10:11:50 +0300

    Добавлены книга и мячик

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,11 @@
 package com.company;
 
+
+
 public class Main {
 
-
     public static void main(String[] args) {
-        Person бездарь = new Person(16, 178);
-        бездарь.setAge(17);//Постарел
-        бездарь.setHeight(180);//Подрос
-        System.out.println(бездарь.getAge());//Ачё сколько тебе там
-        System.out.println(бездарь);//А в целом чё по жизни ?
+
 
     }
 }
